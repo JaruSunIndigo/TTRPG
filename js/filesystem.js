2e4ea915e504148fc60f55327afa9ea61f8c1907
JaruSunIndigo <jkoldaev@gmail.com>
2025-08-31 16:06:02 +0300

    Init

---
+++ js/filesystem.js
@@ -0,0 +1,15 @@
+﻿
+window.localStorageHelper = {
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    },
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    },
+    removeAllItems: function () {
+        localStorage.clear();
+    }
+}
